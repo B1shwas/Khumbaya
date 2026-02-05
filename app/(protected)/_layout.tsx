8c34619114d59528431c01d37e029850fd073f40
Bishwash <bishwash312@gmail.com>
2026-02-05 11:40:09 +0545

    roole check and folder struncture completed

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,22 +1,5 @@
-import { useAuth } from "@/src/store/AuthContext";
-import { Redirect, Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 export default function ProtectedLayout() {
-  const { isAuthenticated } = useAuth();
-
-  if (!isAuthenticated) {
-    return <Redirect href="/(onboarding)" />;
-  }
-
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: "slide_from_right",
-      }}
-    >
-      <Stack.Screen name="(client-tabs)" />
-      <Stack.Screen name="(vendor-tabs)" />
-    </Stack>
-  );
+  return <Slot />;
 }
