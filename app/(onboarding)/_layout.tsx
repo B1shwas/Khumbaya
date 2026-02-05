d534fff2903fec21b4309821ea22e2c17c9afa72
Bishwash <bishwash312@gmail.com>
2026-02-05 11:40:09 +0545

    roole check and folder struncture completed

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -1,23 +1,11 @@
-import { useAuth } from "@/src/store/AuthContext";
-import { Redirect, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function OnboardingLayout() {
-  const { user, isAuthenticated } = useAuth();
-
-  // If logged in, redirect to protected area based on role
-  if (isAuthenticated && user) {
-    if (user.role === "vendor") {
-      return <Redirect href="/(protected)/(vendor-tabs)/home" />;
-    }
-    return <Redirect href="/(protected)/(client-tabs)/home" />;
-  }
-
   return (
     <Stack
       screenOptions={{
         headerShown: false,
         animation: "slide_from_right",
-        animationDuration: 200,
       }}
     >
       <Stack.Screen name="index" />
