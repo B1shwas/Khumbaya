b3508c399a9b96bbc68eaf8d60449f5291872009
syhaminreal <shyaminreallife@gmail.com>
2026-03-09 15:40:55 +0545

    hooks api for check

---
+++ src/features/subevent/api/index.ts
@@ -0,0 +1,2 @@
+export * from "./subEvent.service";
+
---
+++ src/features/subevent/types/index.ts
@@ -0,0 +1,21 @@
+import {
+    SubEventTemplate,
+    TemplateActivity,
+} from "@/src/constants/subeventTemplates";
+
+export interface SelectedActivity {
+  activity: TemplateActivity;
+  time: string;
+  budget: string;
+}
+
+export interface SelectedSubEvent {
+  template: SubEventTemplate;
+  date: string;
+  theme: string;
+  budget: string;
+  activities: SelectedActivity[];
+}
+
+export type { SubEventTemplate, TemplateActivity };
+
